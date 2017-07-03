2ce7a0948df030353bdfe85a30dd7cc670b3ef54
Tony Zhong <tonitick@hotmail.com>
2017-07-03 10:55:52 +0800

    initial trial on data paralellization

---
+++ examples/mnist/train_lenet.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -e
+
+# ./build/tools/caffe train --solver=./examples/mnist/lenet_solver.prototxt $@
+mpirun -np 2 -machinefile /usr/cluster/machinefile /usr/cluster/caffe/build/tools/caffe train --solver=/usr/cluster/caffe/examples/mnist/lenet_solver.prototxt $@
+# mpirun -np 2 /usr/cluster/caffe/build/tools/caffe train --solver=/usr/cluster/caffe/examples/mnist/lenet_solver.prototxt $@
